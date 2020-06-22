53acca43ed1b264f50127ac2fecce00e4be63d60
Tingwei-hub <mrdudys@gmail.com>
2020-06-22 11:53:28 +0800

    Added changes

---
+++ Assets/spawner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawner : MonoBehaviour
+{
+    public GameObject theobject;
+    public GameObject[] myObjects;
+    public float positionY;
+    public float positionZ;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //int randomIndex = Random.Range(0, myObjects.Length);
+        //positionY = Random.Range(4, -4f);
+        // this.transform.position = new Vector3(transform.position.x, positionY, transform.position.z);
+        // Instantiate(myObjects[randomIndex], transform.position, transform.rotation);
+
+
+        int randomIndex = Random.Range(0, myObjects.Length);
+        positionZ = Random.Range(15, 50);
+        this.transform.position = new Vector3(transform.position.x,transform.position.y, positionZ);
+        Instantiate(myObjects[randomIndex], transform.position, transform.rotation);
+
+    }
+}
---
+++ Assets/timedspawned.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class timedspawned : MonoBehaviour
+{
+    public GameObject spawnee;
+    public GameObject spawnee1;
+    public GameObject spawnee2;
+    public GameObject spawnee3;
+
+    public bool stopSpawning = false;
+    public float spawnTime;
+    public float spawnDelay;
+
+    public float spawnCooldown = 1;
+    private float timeUntilSpawn = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnObject", spawnTime, spawnDelay);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeUntilSpawn -= Time.deltaTime;
+        if(timeUntilSpawn <=0)
+        {
+            timeUntilSpawn = spawnCooldown;
+            SpawnObject();
+        }
+    }
+    public void SpawnObject()
+    {
+        Instantiate(spawnee, transform.position, transform.rotation);
+        if (stopSpawning)
+        {
+            CancelInvoke("SpawnObject");
+        }
+       
+    }
+}
